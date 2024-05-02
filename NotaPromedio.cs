3f862d8a93f005be377e4fea386ee9d1faa10906
Katherinerd01 <katherinerdd01@gmail.com>
2024-05-01 20:45:01 -0400

    Programacion Condicional

---
+++ NotaPromedio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+  class Program
+{
+    static void Main(string[] args)
+
+    { 
+        int Nota1, Nota2, Nota3; 
+        Console.Write("Ingrese primer nota:"); 
+        Nota1 = int.Parse(Console.ReadLine()); 
+        Console.Write("Ingrese segunda nota:"); 
+        Nota2 = int.Parse(Console.ReadLine()); 
+        Console.Write("Ingrese tercer nota:"); 
+        Nota3 = int.Parse(Console.ReadLine()); 
+        
+        int Promedio = (Nota1 + Nota2 + Nota3) / 3; 
+
+        if (Promedio >= 7) 
+        { 
+            Console.Write("Promocionado"); 
+        } 
+        else 
+        { 
+            if (Promedio >= 4 && Promedio <7) 
+
+            { Console.Write("Regular"); 
+
+            } 
+            else 
+            { 
+                Console.Write("Reprobado"); 
+            } 
+        }
+        Console.ReadKey(); 
+    }
+}
+
+
+
+
---
+++ Programacion Condicional.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        int num1, num2;
+
+        Console.Write($"Ingrese el primer numero:");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+        Console.Write($"Ingrese el segundo numero:");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+
+        if (num1 > num2)
+
+        {
+            int suma = num1 + num2;
+
+
+            int resta = num1 - num2;
+            Console.Write($"La suma de los dos numeros es:");
+            Console.WriteLine(suma);
+            Console.Write($"La diferencia de los numeros es:");
+            Console.WriteLine(resta);
+        }
+        else
+        {
+           
+            int producto = num1 * num2;
+            int division = num1 / num2;
+            Console.Write($"El producto de los numeros es:");
+            Console.WriteLine(producto);
+            Console.Write($"La division de los dos numeros es:");
+            Console.WriteLine(division);
+
+        }
+    }
+}
+
